b08866b0964553212f05d232bdb1154653df8607
Roy QIU <karoyqiu@gmail.com>
2019-04-30 16:06:04 +0800

    使用最新版本 sentrypad。

--- ports/sentrypad/portfile.cmake
+++ ports/sentrypad/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO karoyqiu/sentrypad
-    REF 5038a75bde498141a48ca916b9cd1128290c9158
-    SHA512 1b82e7cc35f1097db7dfa6fa1f07cdf3a1019360e8d8dca6da0fae8504ce0cd8863e8865013c90b07d40dfaa9eea08340217b727398d819dac307e2c9a1e7aad
+    REF 4a47bcc9da604c98bb2414dcc57c35bb5cc3a5ef
+    SHA512 5c1cf9fc9ca9f34c62b8444f8a0cff9fc1709fafa983d8c4e4fc23c429e89bbae4ea6d1bc23fd4a49827b30ddaf9f9573de7a623a039b326fa4d0b5a95dd0269
     HEAD_REF master
 )
 
